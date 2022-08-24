7f2874c7dfde94a1abe0dc2232f74d829aa2b328
AleMicky <miguel.ale19934@gmail.com>
2022-08-24 10:32:48 -0400

    Funcionalidad encuesta

--- src/pages/encuesta/Pregunta.jsx
+++ src/pages/encuesta/Pregunta.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import {
     Box,
-    Button, Card, CardContent,
-    Container, FormControl,
-    Grid, IconButton, InputLabel, MenuItem,
-    Paper, Select, Stack,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Container,
+    FormControl,
+    Grid,
+    IconButton,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    MenuItem,
+    Paper,
+    Select,
+    Stack,
     Step,
     StepLabel,
     Stepper,
@@ -12,10 +25,16 @@ import {
     Toolbar,
     Typography
 } from "@mui/material";
-import ImageIcon from '@mui/icons-material/Image';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import DoNotDisturbOnIcon from '@mui/icons-material/DoNotDisturbOn';
 
+const Encuesta = ({ encuesta, onEncuesta }) => {
 
-const Encuesta = () => {
+    const { nombre, descripcion } = encuesta;
+
     return (
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
@@ -28,6 +47,8 @@ const Encuesta = () => {
                         name="nombre"
                         label="Nombre"
                         fullWidth
+                        value={nombre}
+                        onChange={(e) => onEncuesta(e.target.value, 'nombre')}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -36,22 +57,9 @@ const Encuesta = () => {
                         name="descripcion"
                         label="Descripción"
                         fullWidth
+                        value={descripcion}
+                        onChange={(e) => onEncuesta(e.target.value, 'descripcion')}
                     />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-standard-label">Entidad</InputLabel>
-                        <Select
-                            label="Entidad"
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard">
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={1}>FarmaCoorp</MenuItem>
-                            <MenuItem value={2}>Helias</MenuItem>
-                        </Select>
-                    </FormControl>
                 </Grid>
             </Grid>
         </React.Fragment>
@@ -61,54 +69,105 @@ const Encuesta = () => {
 
 
 
-function CardPregunta() {
+function CardPregunta({ pregunta, nuevo, elminar, actualizar, nuevoResp, deleteRespuesta }) {
 
-    const [tipoRespuesta, setTipoRespuesto] = useState('opcion');
+    const { id, nombre, respuestas, tipoRespuesta } = pregunta;
+
+    const [tipo, setTipo] = useState(tipoRespuesta);
 
     const handleChangeSelect = (event) => {
-        setTipoRespuesto(event.target.value);
+        setTipo(event.target.value);
     };
 
     return (
         <Card>
             <CardContent>
                 <Stack direction="row" spacing={1}>
-                    <TextField fullWidth
+                    <TextField
+                        fullWidth
                         label="Pregunta"
-                        variant="filled" />
-
+                        variant="outlined"
+                        value={nombre}
+                        onChange={e => actualizar(id, e.target.value)}
+                    />
                     <Box sx={{ minWidth: 260 }}>
                         <FormControl fullWidth>
                             <InputLabel>Varias opciones</InputLabel>
                             <Select
-                                value={tipoRespuesta}
+                                value={tipo}
                                 label="Tipo Respuesta"
-                                variant="filled"
+                                variant="outlined"
                                 onChange={handleChangeSelect}>
                                 <MenuItem value={'opcion'}>Varias opciones</MenuItem>
                                 <MenuItem value={'casiilla'}>Casilla</MenuItem>
+                                <MenuItem value={'texto'}>Texto Corto</MenuItem>
                             </Select>
                         </FormControl>
                     </Box>
                 </Stack>
-
-
-                
+                <Box sx={{ width: '100%', maxWidth: 300, bgcolor: 'background.paper' }}>
+                    <List>
+                        {
+                            respuestas.map((resp, i) => (
+                                <ListItem disablePadding key={resp.id}>
+                                    <ListItemIcon>
+                                        <RadioButtonUncheckedIcon />
+                                    </ListItemIcon>
+                                    <Box sx={{ minWidth: 600, marginBlockEnd: 1 }}>
+                                        <TextField
+                                            fullWidth
+                                            label={`Opcion ${i + 1}`}
+                                             variant="outlined"
+                                        />
+                                    </Box>
+                                    <IconButton
+                                        aria-label="delete"
+                                        onClick={() => deleteRespuesta(id, resp.id)}
+                                        color="error"
+                                        size='large'>
+                                        <DoNotDisturbOnIcon />
+                                    </IconButton>
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                    <IconButton aria-label="add" color="primary" onClick={() => nuevoResp(id)}>
+                        <AddBoxIcon />
+                    </IconButton>
+                </Box>
             </CardContent>
+            <CardActions>
+                <IconButton aria-label="nuevo" onClick={nuevo} size="large" color="success">
+                    <AddCircleIcon />
+                </IconButton>
+                <IconButton aria-label="elminar" onClick={() => elminar(id)} size="large" color="error">
+                    <DeleteIcon />
+                </IconButton>
+            </CardActions>
         </Card>
     );
 }
 
-const PreguntaRespuesta = () => {
+const PreguntaRespuesta = ({ encuesta, nuevo, elminar, actualizar, nuevoRespuesta, deleteRespuesta }) => {
+    const { pregunta } = encuesta;
     return (
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
                 Pregunta
             </Typography>
             <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <CardPregunta />
-                </Grid>
+                {
+                    pregunta.map((pre) => (
+                        <Grid item xs={12} key={pre.id} >
+                            <CardPregunta pregunta={pre}
+                                nuevo={nuevo}
+                                elminar={elminar}
+                                actualizar={actualizar}
+                                nuevoResp={nuevoRespuesta}
+                                deleteRespuesta={deleteRespuesta} />
+                        </Grid>
+                    ))
+                }
             </Grid>
         </React.Fragment>
     )
@@ -128,14 +187,32 @@ const Resumen = () => {
 
 const steps = ['Encuesta', 'Pregunta', 'Resumen'];
 
-function getStepContent(step) {
+function getStepContent(
+    step,
+    encuesta,
+    onEncuesta,
+    newPregunta,
+    deletePregunta,
+    updatePregunta,
+    newRespuesta,
+    deleteRespuesta) {
     switch (step) {
         case 0:
-            return <Encuesta />;
+            return <Encuesta
+                encuesta={encuesta}
+                onEncuesta={onEncuesta}
+            />;
         case 1:
-            return <PreguntaRespuesta />;
+            return <PreguntaRespuesta
+                encuesta={encuesta}
+                nuevo={newPregunta}
+                elminar={deletePregunta}
+                actualizar={updatePregunta}
+                nuevoRespuesta={newRespuesta}
+                deleteRespuesta={deleteRespuesta}
+            />;
         case 2:
-            return <Resumen />;
+            return <Resumen encuesta={encuesta} />;
         default:
             throw new Error('Unknown step');
     }
@@ -143,20 +220,129 @@ function getStepContent(step) {
 
 export const Pregunta = () => {
 
+    const initialState = {
+        nombre: '',
+        descripcion: '',
+        pregunta: [
+            {
+                id: uuidv4(),
+                nombre: '',
+                tipoRespuesta: 'opcion',
+                respuestas: [
+                    {
+                        id: uuidv4(),
+                        nombre: ''
+                    }
+                ]
+            }
+        ]
+    };
+
     const [activeStep, setActiveStep] = useState(0);
+
+    const [encuesta, setEncuesta] = useState(initialState);
+
+    const onEncuesta = (value, field) => {
+        setEncuesta({
+            ...encuesta,
+            [field]: value
+        });
+    }
+
+    const newPregunta = () => {
+        const prevEncuesta = { ...encuesta };
+        prevEncuesta.pregunta.push({
+            id: uuidv4(),
+            nombre: '',
+            tipoRespuesta: 'opcion',
+            respuestas: [{
+                id: uuidv4(),
+                nombre: ''
+            }]
+        });
+        setEncuesta(prevEncuesta);
+    }
+
+
+    const deletePregunta = (id) => {
+        let prevEncuesta = { ...encuesta };
+        const arrayPregunta = prevEncuesta.pregunta.filter((item) => item.id !== id);
+        prevEncuesta.pregunta = arrayPregunta;
+        setEncuesta(prevEncuesta);
+    }
+
+    const deleteRespuesta = (idPregunta, idRespuesta) => {
+
+        let prevEncuesta = { ...encuesta };
+
+        const index = prevEncuesta.pregunta.findIndex(p => p.id === idPregunta);
+
+        const { respuestas, ...preguntaDetalle } = prevEncuesta.pregunta[index];
+
+        const newPregunta = {
+            ...preguntaDetalle,
+            respuestas: respuestas.filter((item) => item.id !== idRespuesta)
+        }
+        prevEncuesta.pregunta[index] = newPregunta;
+
+
+        setEncuesta(prevEncuesta);
+
+    }
+
+
+    const newRespuesta = (id) => {
+        let prevEncuesta = { ...encuesta };
+        const index = prevEncuesta.pregunta.findIndex(p => p.id === id);
+        const { respuestas, ...preguntaDetalle } = prevEncuesta.pregunta[index];
+        const newPregunta = {
+            ...preguntaDetalle,
+            respuestas: [...respuestas, { id: uuidv4(), nombre: '', }]
+        }
+        prevEncuesta.pregunta[index] = newPregunta;
+        setEncuesta(prevEncuesta);
+    }
+
+    const updatePregunta = (idPregunta, pregunta) => {
+
+        let prevEncuesta = { ...encuesta };
+
+        const pregutan = prevEncuesta.pregunta.map((obj) => {
+            if (obj.id === idPregunta) {
+                return { ...obj, nombre: pregunta }
+            }
+            return obj
+        })
+
+        prevEncuesta.pregunta = pregutan;
+
+        setEncuesta(prevEncuesta);
+
+    }
+
+    const updateRespuesta = (idPregunta, idRespuesta, respuesta) => {
+        let prevEncuesta = { ...encuesta };
+        const indexPregunta = prevEncuesta.pregunta.findIndex(p => p.id === idPregunta);
+        console.log(indexPregunta);
+    }
+
 
     const handleNext = () => {
         setActiveStep(activeStep + 1);
+        if (activeStep === 2) {
+            console.log(encuesta);
+        }
     };
 
     const handleBack = () => {
         setActiveStep(activeStep - 1);
     };
+
     return (
         <React.Fragment>
             <Toolbar />
-            <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-                <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+            <Container maxWidth="lg" sx={{ mb: 4 }}>
+                <Paper variant="elevation" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                     <Typography component="h1" variant="h4" align="center">
                         Nuevo Encuesta
                     </Typography>
@@ -171,29 +357,32 @@ export const Pregunta = () => {
                         {activeStep === steps.length ? (
                             <React.Fragment>
                                 <Typography variant="h5" gutterBottom>
-                                    Thank you for your order.
+                                    Encuesta Creada
                                 </Typography>
                                 <Typography variant="subtitle1">
-                                    Your order number is #2001539. We have emailed your order
-                                    confirmation, and will send you an update when your order has
-                                    shipped.
+                                    Exito
                                 </Typography>
                             </React.Fragment>
                         ) : (
                             <React.Fragment>
-                                {getStepContent(activeStep)}
+                                {getStepContent(
+                                    activeStep,
+                                    encuesta,
+                                    onEncuesta,
+                                    newPregunta,
+                                    deletePregunta,
+                                    updatePregunta,
+                                    newRespuesta,
+                                    deleteRespuesta)}
                                 <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                                     {activeStep !== 0 && (
                                         <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
                                             Atras
                                         </Button>
                                     )}
-
-                                    <Button
-                                        variant="contained"
+                                    <Button variant="contained"
                                         onClick={handleNext}
-                                        sx={{ mt: 3, ml: 1 }}
-                                    >
+                                        sx={{ mt: 3, ml: 1 }}   >
                                         {activeStep === steps.length - 1 ? 'Realizar Encuesta' : 'Siguente'}
                                     </Button>
                                 </Box>
